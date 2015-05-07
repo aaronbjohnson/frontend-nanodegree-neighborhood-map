19d3b887a8466af37fcdd149634299c42502d885
Aaron <aj65461@gmail.com>
2015-05-07 17:53:10 -0400

    Add comments
    Implement panTo, so that a click on an item list or marker pans to that location

--- develop/js/app.js
+++ develop/js/app.js
@@ -54,6 +54,11 @@ var initMap = function() {
     // Add image for custom map icon
     var image = 'develop/images/star.png';
 
+    /*
+     * Loop over the initialPlaces and create a Google Map marker for each item. 
+     * Push these markers to the gmarkers array.
+     * http://stackoverflow.com/questions/15531390/adding-array-of-markers-in-google-map
+     */
     var i;
     for (i = 0; i < initialPlaces.length; i++) {
         marker = new google.maps.Marker({
@@ -63,10 +68,10 @@ var initMap = function() {
             map: map,
             icon: image
         });
-        // removed i from function arguments
+
         google.maps.event.addListener(marker, 'click', (function(marker) {
             return function() {
-                // div content not working
+                map.panTo(marker.position);
                 infowindow.setContent(marker.title + "<div id='content'></div>");
                 infowindow.open(map, marker);
                 apiRequest(marker);
@@ -84,6 +89,14 @@ var foursquareApi = 'https://api.foursquare.com/v2/venues/search?client_id=' +
     'NJFWJLYRXMAHO2W2F1SIGOTA5LMHMSUTGLM2XBRAXV5YMUBM&v=20150401';
 var googleStreetview = 'https://maps.googleapis.com/maps/api/streetview?size=' + 
     '400x200&location=';
+
+/*
+ * Sends an API request to Foursquare and returns the venue's phone number, 
+ * address, street number, city and country. This also sends a Google Street
+ * View API request and returns a street view of that location based off of the 
+ * street number, city, and country -- provided by the Foursquare API. This
+ * information is then used to populate an infowindow. 
+ */
 
 var apiRequest = function(marker) {
     var lat = marker.position.lat(); 
@@ -126,42 +139,60 @@ var ViewModel = function() {
     self.placeList = ko.observableArray(initialPlaces);
 
     self.markers = ko.observableArray(gmarkers);
-    // got this from neighborhood explorer master repository
+
     self.filter = ko.observable('');
 
     /* 
-     * Create a function that will open a marker's infowindow when an item from
-     * the list is clicked on.
+     * Create a function that will open a marker's infowindow when the user 
+     * clicks on an item from the list. Also, pans the map view to center on the
+     * marker.
      */
     self.openWindow = function(initialPlaces) {
         var pin = gmarkers[initialPlaces.pinID];
+        var latLng = new google.maps.LatLng(initialPlaces.lat, initialPlaces.lng);
 
+        map.panTo(latLng);
         infowindow.open(map, pin);
         infowindow.setContent(pin.title + '<div id="content"></div>');
         apiRequest(pin);
     }
 
-    /*
-     * 
-     */
-    self.toggleMarkers = function(state) {
+    // Function that will be used to turn the gmarkers on or off.
+
+    self.toggleMarkers = function(toggle) {
         for (var i = 0; i < gmarkers.length; i++) {
-            gmarkers[i].setMap(state);
+            gmarkers[i].setMap(toggle);
         };
     }
 
+    /* 
+     * Function that uses Knockout's arrayFilter utility function to pass in the
+     * placeList array and control which items are filtered based on the result
+     * of the function -- executed on each placeList item -- that returns those
+     * items who's name matches the search input.
+     * http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
+     */
+
     self.filterArray = function(filter) {
-        return ko.utils.arrayFilter(self.placeList(), function(location) {
-            return location.name.toLowerCase().indexOf(filter) >= 0;
+        return ko.utils.arrayFilter(self.placeList(), function(item) {
+            return item.name.toLowerCase().indexOf(filter) >= 0;
         })
     }
 
+    /*
+     * After filtering the items, the displaySelected function calls setMap on 
+     * those items -- which display's their markers on the map.
+     */
     self.displaySelected = function(filteredmarkers) {
         for (var i = 0; i < filteredmarkers.length; i++) {
             gmarkers[filteredmarkers[i].pinID].setMap(map);
         };
     }
 
+    /*
+     * This function checks to see if there is anything to be filtered. If 
+     * there's nothing to be filtered, it will display the entire item list.
+     */
     self.filterList = function() {
         var filter = self.filter().toLowerCase();
         if (!filter) {
@@ -176,6 +207,8 @@ var ViewModel = function() {
         }
     }
 };
+
+
 
 google.maps.event.addDomListener(window, 'load', initMap);
 
