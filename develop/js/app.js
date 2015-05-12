735b3b8a9ba80c745d43c4957890feb4533f3a42
Aaron <aj65461@gmail.com>
2015-05-12 11:34:20 -0400

    Commit before refactor to hopefully access map from ViewModel.openWindow

--- develop/js/app.js
+++ develop/js/app.js
@@ -1,3 +1,7 @@
+var map, infowindow;
+
+var gmarkers = [];
+
 ko.bindingHandlers.googlemap = {
     init: function (element, valueAccessor) {
         var
@@ -32,11 +36,12 @@ ko.bindingHandlers.googlemap = {
                     map.panTo(marker.position);
                     infowindow.setContent(marker.title + "<div id='content'></div>");
                     infowindow.open(map, marker);
-                    apiRequest(marker);
+                    // apiRequest(marker);
                 };
             })(marker));
 
-
+            // Push the marker to the gmarkers array
+            gmarkers.push(marker);
         }
     }
 };
@@ -44,27 +49,33 @@ ko.bindingHandlers.googlemap = {
 var ViewModel = function() {
     // Data
     var self = this;
+    
     self.locations = ko.observableArray([
-        {name: "Soda Bar", latitude:40.678396 , longitude:-73.968349},
-        {name: "Brooklyn Roasting Co.", latitude: 40.704334, longitude: -73.986524}
+        {name: "Soda Bar", latitude:40.678396 , longitude:-73.968349, pinId:0},
+        {name: "Brooklyn Roasting Co.", latitude: 40.704334, longitude: -73.986524, pinId:1}
     ]);
+
+    self.markers = ko.observable(gmarkers);
     self.chosenLocationId = ko.observable();
     self.filter = ko.observable('');
-
 
     // Behaviors
     self.goToLocation = function(location) {
         self.chosenLocationId(location);
-        //new trying
-        var latlng = new google.maps.LatLng(locations.latitude, + locations.longitude);
-
-        map.panTo(latlng);
-        infowindow.open(map, chosenLocationId);
-        infowindow.setContent(chosenLocationId.title + '<div id="content"></div>');
-        apiRequest(chosenLocationId);
     };
 
-    /* Create a thing*/
+    self.openWindow = function(locations) {
+        //new trying
+        var pin = gmarkers[locations.pinId];
+        //var latLng = new google.maps.LatLng(locations.latitude, locations.longitude);
+        console.log(map);
+        //map.panTo(latLng);
+        infowindow.open(map, pin);
+        infowindow.setContent(pin.title + '<div id="content"></div>');
+        // apiRequest(pin);
+    };
+
+    // Search filter
     self.filteredItems = ko.computed(function() {
         var filter = self.filter().toLowerCase();
         if (!filter) {
@@ -75,20 +86,6 @@ var ViewModel = function() {
             });
         }
     }, self);
-/*
-// Search Function
-
-    self.firstMatch = ko.computed(function() {
-        var search = this.search().toLowerCase();
-        if (!search) {
-            return null;
-        } else {
-            return ko.utils.arrayFirst(this.filteredItems(), function(item) {
-                return location.name.toLowerCase().indexOf(search) !== -1;
-            });
-        }
-    }, self);
-*/
 };
     
 ko.applyBindings(new ViewModel());
