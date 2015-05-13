b1209bb3cf45078d4ad4a6435a5d47a77c4b3cf3
Aaron <aj65461@gmail.com>
2015-05-13 17:42:30 -0400

    Bring in Api functionality from old branch
    Bring Bootstrap grid system back

--- develop/js/app.js
+++ develop/js/app.js
@@ -8,97 +8,9 @@ var foursquareApi = 'https://api.foursquare.com/v2/venues/search?client_id=' +
 var googleStreetview = 'https://maps.googleapis.com/maps/api/streetview?size=' + 
     '300x150&location=';
 
-
-
-
-
-var apiRequest = function(marker) {
-    "use strict";
-    var lat = marker.position.lat(); 
-    var lng = marker.position.lng();
-
-    var $windowContent = $('#content');
-
-    var url = foursquareApi + '&ll=' + lat + ',' + lng + '&query=\'' + 
-        marker.title + '\'&limit=1';
-
-    $.getJSON(url, function(response) {
-        var venue = response.response.venues[0];
-        var venuePhone = venue.contact.formattedPhone;
-        var venueAddress = venue.location.formattedAddress;
-        var venueStreet = venue.location.address;
-        var venueCity = venue.location.city;
-        var venueCountry = venue.location.country;
-        var venueLocation = venueStreet + venueCity + venueCountry;
-        var streetPhotoUrl = googleStreetview + venueLocation;
-
-        // Handles undefined error if phone number cannot be found
-        if (venuePhone === undefined) {
-            venuePhone = '<i>Could not find phone number for this location<i>';
-        } else {
-            venuePhone = venuePhone;
-        }
-
-        $windowContent.append('<p>' + venuePhone + '</p>');
-        $windowContent.append('<p>' + venueAddress + '</p>');
-        $windowContent.append('<img class="street-img" src="' + streetPhotoUrl +
-            '">');
-    }).error(function(err) {
-        $windowContent.text('No information can be retrieved at this time.');
-    });
-};
-
-
-
-
-
-
-
-ko.bindingHandlers.googlemap = {
-    init: function (element, valueAccessor) {
-        var
-          value = valueAccessor(),
-          mapOptions = {
-            zoom: 12,
-            center: new google.maps.LatLng(value.centerLat, value.centerLon),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-            }
-          map = new google.maps.Map(element, mapOptions);
-
-          infowindow = new google.maps.InfoWindow();
-
-          var image = 'develop/images/star.png';
-          
-        for (var l in value.locations())
-        {
-            var latLng = new google.maps.LatLng(
-                            value.locations()[l].latitude, 
-                            value.locations()[l].longitude);
-            var marker = new google.maps.Marker({
-                position: latLng,
-                map: map,
-
-                title: value.locations()[l].name,
-                icon: image
-            });
-
-            google.maps.event.addListener(marker, 'click', (function(marker) {
-                return function() {
-                    map.panTo(marker.position);
-                    infowindow.setContent(marker.title + "<div id='content'></div>");
-                    infowindow.open(map, marker);
-                    // apiRequest(marker);
-                };
-            })(marker));
-
-            // Push the marker to the gmarkers array
-            gmarkers.push(marker);
-        }
-    }
-};
-
 var ViewModel = function() {
     // Data
+
     var self = this;
     
     self.locations = ko.observableArray([
@@ -108,12 +20,94 @@ var ViewModel = function() {
 
     self.markers = ko.observable(gmarkers);
     self.chosenLocationId = ko.observable();
-    // Define a chosenApiData property on ViewModel
-    self.chosenApiData = ko.observable();
     self.filter = ko.observable('');
 
     // Behaviors
 
+    self.apiRequest = function(marker) {
+        "use strict";
+        var lat = marker.position.lat(); 
+        var lng = marker.position.lng();
+
+        var $windowContent = $('#content');
+
+        var url = foursquareApi + '&ll=' + lat + ',' + lng + '&query=\'' + 
+            marker.title + '\'&limit=1';
+
+        $.getJSON(url, function(response) {
+            var venue = response.response.venues[0];
+            var venuePhone = venue.contact.formattedPhone;
+            var venueAddress = venue.location.formattedAddress;
+            var venueStreet = venue.location.address;
+            var venueCity = venue.location.city;
+            var venueCountry = venue.location.country;
+            var venueLocation = venueStreet + venueCity + venueCountry;
+            var streetPhotoUrl = googleStreetview + venueLocation;
+
+            // Handles undefined error if phone number cannot be found
+            if (venuePhone === undefined) {
+                venuePhone = '<i>Could not find phone number for this location<i>';
+            } else {
+                venuePhone = venuePhone;
+            }
+
+            $windowContent.append('<p>' + venuePhone + '</p>');
+            $windowContent.append('<p>' + venueAddress + '</p>');
+            $windowContent.append('<img class="street-img" src="' + streetPhotoUrl +
+                '">');
+        }).error(function(err) {
+            $windowContent.text('No information can be retrieved at this time.');
+        });
+    };
+
+    // Making a Knockout binding handler to create the map.
+    ko.bindingHandlers.googlemap = {
+        init: function (element, valueAccessor) {
+            var
+              value = valueAccessor(),
+              mapOptions = {
+                zoom: 12,
+                center: new google.maps.LatLng(value.centerLat, value.centerLon),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+              map = new google.maps.Map(element, mapOptions);
+
+              infowindow = new google.maps.InfoWindow();
+
+              var image = 'develop/images/star.png';
+              
+            for (var l in value.locations())
+            {
+                var latLng = new google.maps.LatLng(
+                                value.locations()[l].latitude, 
+                                value.locations()[l].longitude);
+                var marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map,
+
+                    title: value.locations()[l].name,
+                    icon: image
+                });
+
+                google.maps.event.addListener(marker, 'click', (function(marker) {
+                    return function() {
+                        map.panTo(marker.position);
+                        infowindow.setContent(marker.title + "<div id='content'></div>");
+                        infowindow.open(map, marker);
+                        self.apiRequest(marker);
+                    };
+                })(marker));
+
+                // Push the marker to the gmarkers array
+                gmarkers.push(marker);
+            }
+        }
+    };
+    
+    /**
+     * Function that will open a marker's infowindow when the user clicks on an
+     * item from the list.
+     */
     self.openWindow = function(locations) {
         var lat = locations.latitude;
         var lng = locations.longitude;
@@ -123,32 +117,59 @@ var ViewModel = function() {
         var url = foursquareApi + '&ll=' + lat + ',' + lng + '&query=\'' + 
             pin.title + '\'&limit=1';
 
-
         self.chosenLocationId(locations);
-        // When user clicks on list item, populate chosenApiData by performing an Ajax request:
-        $.get(url, {}, self.chosenApiData);
 
-
-
-        //var latLng = new google.maps.LatLng(locations.latitude, locations.longitude);
-        //map.panTo(latLng);
         infowindow.open(map, pin);
-        // we may need to remove the <div===content>> stuff out of here because
-        // we may be putting this stuff on the View (HTML)...after all, that was
-        // our problem before right? Right?
         infowindow.setContent(pin.title + '<div id="content"></div>');
-        // apiRequest(pin);
+        self.apiRequest(pin);
     };
 
-    // Search filter
+    // Function that will be used to turn the gmarkers on or off.
+    self.toggleMarkers = function(toggle) {
+        for (var i = 0; i < gmarkers.length; i++) {
+            gmarkers[i].setMap(toggle);
+        }
+    };
+
+    /**
+     * Function that uses Knockout's arrayFilter utility function to pass in the
+     * placeList array and control which items are filtered based on the result
+     * of the function -- executed on each placeList item -- that returns those
+     * items who's name matches the search input.
+     * http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
+     */
+    self.filterArray = function(filter) {
+        return ko.utils.arrayFilter(self.locations(), function(location) {
+            return location.name.toLowerCase().indexOf(filter) !== -1;
+        });
+    };
+
+    /**
+     * After filtering the items, the displaySelected function calls setMap on 
+     * those items -- which display's their markers on the map.
+     */
+    self.displaySelected = function(filteredMarkers) {
+        for (var i = 0; i < filteredMarkers.length; i++) {
+            gmarkers[filteredMarkers[i].pinId].setMap(map);
+        }
+    };
+
+    /**
+     * This function checks to see if there is anything to be filtered. If 
+     * there's nothing to be filtered, it will display the entire item list and
+     * all the markers.
+     */
     self.filteredItems = ko.computed(function() {
         var filter = self.filter().toLowerCase();
         if (!filter) {
+            self.toggleMarkers(map);
             return self.locations();
         } else {
-            return ko.utils.arrayFilter(self.locations(), function(location) {
-                return location.name.toLowerCase().indexOf(filter) !== -1;
-            });
+            self.toggleMarkers(null);
+            var filteredMarkers = [];
+            filteredMarkers = self.filterArray(filter);
+            self.displaySelected(filteredMarkers);
+            return filteredMarkers;
         }
     }, self);
 };
